38160a53c652857329e97906b98f3436a655281e
Jorge Meireles Junior <104947075+MeirelesJunior@users.noreply.github.com>
2023-07-07 19:39:43 -0300

    Inserindo uma ContaBanco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        
+        Scanner leitor = new Scanner(System.in);
+
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.println("Por favor, informe o seu nome:");
+        nomeCliente = leitor.next();
+
+        System.out.println("Informe a sua agência:");
+        agencia = leitor.next();
+
+        System.out.println("Informe o número da sua conta:");
+        numeroConta = leitor.nextInt();
+
+        System.out.println("Informe o seu saldo:");
+        saldo = leitor.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponivel para saque.");
     }
-    
 }
